3defdcf8bde0e043a93964d359637a2342dc6e68
Joao Jurema <joaoalvesjurema@gmail.com>
2021-06-08 19:47:12 -0300

    Aula 01 done.

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,23 @@
 const express = require('express'); // biblioteca para comandos do servidor
 const routes = express.Router(); //vai criar as rotas
 
-const views = __dirname + "/views/";//__dirname - diretorio principal = /src/
+const views = __dirname + "/views/";//__dirname - diretorio atual = /src/
+
+const profile = {
+    name: "João Jurema",
+    avatar: "https://avatars.githubusercontent.com/u/11191464?v=4",
+    "monthly-budget": 3000, //variáveis com traço requerem aspas
+    "hours-per-day": 8,
+    "days-per-week": 5,
+    "vacation-per-year": 8
+
+}
 
 // require, response
-routes.get('/', (req, res) => res.render(views + 'index.ejs'));
-routes.get('/job', (req, res) => res.render(views + 'job.ejs'));
-routes.get('/job/edit', (req, res) => res.render(views + 'job-edit.ejs'));
-routes.get('/profile', (req, res) => res.render(views + 'profile.ejs'));
+routes.get('/', (req, res) => res.render(views + 'index'));
+routes.get('/job', (req, res) => res.render(views + 'job'));
+routes.get('/job/edit', (req, res) => res.render(views + 'job-edit'));
+routes.get('/profile', (req, res) => res.render(views + 'profile', {profile: profile}));
 
 
 
--- src/server.js
+++ src/server.js
@@ -1,9 +1,14 @@
+
+
 const express = require("express");
 const server = express();
-const routes = require(__dirname + "/routes");
+const routes = require("./routes");
+
+server.use(express.static('../public')); //linha tirada de https://stackoverflow.com/questions/57671753/referenceerror-app-is-not-defined-node-js
+// serve para guiar  o app de onde pegar os css
 
 //usando template engine
-server.set('view-engine', 'ejs');
+server.set('view engine', 'ejs');
 
 //habilitar arquivos statics
 server.use(express.static("public"));
