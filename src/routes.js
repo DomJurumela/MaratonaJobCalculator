ce330fd9253298a5b67c2c26e4ef8d2bcd6d110a
Joao Jurema <joaoalvesjurema@gmail.com>
2021-06-09 10:33:20 -0300

    Aula 2 at 1h20min

--- src/routes.js
+++ src/routes.js
@@ -13,9 +13,28 @@ const profile = {
 
 }
 
+const jobs = [];//array armazenará todos os jobs
+
+ 
 // require, response
 routes.get('/', (req, res) => res.render(views + 'index'));
 routes.get('/job', (req, res) => res.render(views + 'job'));
+routes.post('/job', (req, res) => {    //POST será para receber dados do formulário em EJS
+    //req.body={ name: 'sdasdadasd', 'daily-hours': '0.6', 'total-hours': '6' }
+    //req.body contém os arquivos contidos no formulário
+    
+    const lastId = jobs[jobs.length - 1]?.id || 0; //vai criar um novo valor de id
+    //Pegará o valor do índice do último id no vetor ou 0, se o vetor estiver vazio
+
+    jobs.push({
+        id: lastId + 1, //pegará o valor posterior ao id do último elemento do array
+        name: req.body.name,
+        "daily-hours": req.body["daily-hours"],
+        "total-hours": req.body["total-hours"],
+        created_at: Date.now() //data atual
+    }); 
+    return res.redirect('/') //voltando à página inicial
+})
 routes.get('/job/edit', (req, res) => res.render(views + 'job-edit'));
 routes.get('/profile', (req, res) => res.render(views + 'profile', {profile: profile}));
 
--- src/server.js
+++ src/server.js
@@ -1,17 +1,20 @@
-
+//comando terminal para rodar o código: "npm run dev"
 
 const express = require("express");
 const server = express();
 const routes = require("./routes");
 
-server.use(express.static('../public')); //linha tirada de https://stackoverflow.com/questions/57671753/referenceerror-app-is-not-defined-node-js
+//server.use(express.static('../public')); //linha tirada de https://stackoverflow.com/questions/57671753/referenceerror-app-is-not-defined-node-js
 // serve para guiar  o app de onde pegar os css
 
 //usando template engine
 server.set('view engine', 'ejs');
 
-//habilitar arquivos statics
+//habilitar arquivos statics (Pode bugar)
 server.use(express.static("public"));
+
+//liberar o uso o req.body
+server.use(express.urlencoded({ extended: true }));
 
 //routes
 server.use(routes);
